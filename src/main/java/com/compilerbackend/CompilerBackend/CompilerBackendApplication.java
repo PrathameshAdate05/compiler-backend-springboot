a57780ea7a72474e5bbf6d4fd784abc4ce21fc48
Prathamesh Adate <prathameshadate05@gmail.com>
2023-11-30 11:18:26 +0530

    Project setup

---
+++ src/main/java/com/compilerbackend/CompilerBackend/CompilerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.compilerbackend.CompilerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompilerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompilerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compilerbackend/CompilerBackend/CompilerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compilerbackend.CompilerBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompilerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
